a2b090ab1ec809a1e9e3ac6757a90584fd6d8146
jonayGodoy <jonaydam2@gmail.com>
2017-04-02 18:45:21 +0100

    repair loadJson

--- src/ManagerFile.js
+++ src/ManagerFile.js
@@ -1,17 +1,19 @@
 'use strict';
 module.exports = function ManagerFile() {
      this.loadJson = function(){
-        let httpRequest = new XMLHttpRequest();
-        httpRequest.onreadystatechange = function() {
-            if (httpRequest.readyState === 4) {
-                if (httpRequest.status === 200) {
-                    let data = httpRequest.responseText;
-                    localStorage.setItem('preguntas',data);
-                   }
-            }
-        };
-        httpRequest.open('get', '../public/preguntas.json');
-        httpRequest.send();
-         return JSON.parse(localStorage.getItem("preguntas"));
+         return new Promise((resolve, reject) => {
+             var data = "";
+            let httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = function() {
+                if (httpRequest.readyState === 4) {
+                    if (httpRequest.status === 200) {
+                         data = httpRequest.responseText;
+                        resolve(JSON.parse(data));
+                       }
+                }
+            };
+            httpRequest.open('get', '../public/preguntas.json');
+            httpRequest.send();
+         });
     }
 };
--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ let ManagerDom = require('./ManagerDom');
 initialise();
 function initialise(){
     let managerFile = new ManagerFile();
-    let listJson = managerFile.loadJson();
-    let managerQuiz = new ManagerQuiz(listJson.preguntas);
-    let managerDom = new ManagerDom();
-    let game = new Game(managerQuiz,managerDom);
+     managerFile.loadJson().then(v => {
+        let managerQuiz = new ManagerQuiz(v.preguntas);
+        let managerDom = new ManagerDom();
+        let game = new Game(managerQuiz,managerDom);
 
-    window.start = game.startGame;
-    window.updateAnswer = game.updateAnswer;
-    window.nextQuestion = game.nextQuestion;
+        window.start = game.startGame;
+        window.updateAnswer = game.updateAnswer;
+        window.nextQuestion = game.nextQuestion;
+    });
+
+
 }
 
 window.resetGame = function(){
